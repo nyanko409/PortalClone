26c2b43c206b68fe35233c23fb8d31d545cc460a
ato <ishimoto_ato@me.com>
2021-02-26 20:33:44 +0900

    adjust angled surface collider

--- DX11Base/stage.cpp
+++ DX11Base/stage.cpp
@@ -52,7 +52,7 @@ void Stage::Init()
 
 	// angled surface
 	m_colliders.push_back(new PolygonCollider());
-	m_colliders.back()->Init(this, { 8.0f, -3, 15.0f }, { -2.8f, -3, 15.0f }, { -2.8f, -11.5f, 6.5f }, { 8.0f, -11.5f, 6.5f }); // x angled
+	m_colliders.back()->Init(this, { 8.0f, -3, 14.9f }, { -2.8f, -3, 14.9f }, { -2.8f, -11.5f, 6.4f }, { 8.0f, -11.5f, 6.4f }); // x angled
 	m_colliders.push_back(new PolygonCollider());
 	m_colliders.back()->Init(this, { 27.1f, 18, 7.9f }, { 18.2f, 18, 16.8f }, { 18.2f, -11.7f, 16.8f }, { 27.1f, -11.7f, 7.9f }); // y angled
 }
